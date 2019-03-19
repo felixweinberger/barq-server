f5550e945d120c5f610ed25c47c9ae5f7c4ea41f
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-19 15:18:38 +0100

    feat(customer): add customer routes and implement mock API versions

--- controllers/customer/index.js
+++ controllers/customer/index.js
@@ -1,18 +1,57 @@
+/* eslint-disable no-console */
+
+import stripeCharger from 'stripe';
 import customerModel from '../../models/customer';
 
-const ctrl = {};
+const customerCtrl = {};
 
-ctrl.getMenu = async (req, res) => {
-  const { barId } = req.params;
-  console.log('barId: ', barId);
-  customerModel.getMenu(barId);
-  res.sendStatus(200);
+const stripeAccount = stripeCharger(process.env.STRIPE_KEY);
+
+const mockPaymentConfirmation = {
+  orderId: null,
+  status: 'paid',
+  items: [
+    {
+      name: 'Corona',
+      price: 3.6,
+      quantity: 2,
+    },
+    {
+      name: 'Becks',
+      price: 3.4,
+      quantity: 1,
+    },
+  ],
 };
 
-ctrl.pay = async (req, res) => {
+customerCtrl.getMenu = async (req, res) => {
   const { barId } = req.params;
-  console.log('barId: ', barId);
-  res.sendStatus(200);
+  try {
+    const menu = await customerModel.getMenu(barId);
+    res.status(200);
+    res.send(menu);
+  } catch (e) {
+    console.log('Something went wrong while fetching the menu');
+    res.status(500);
+    res.send({ Error: 'Something went wrong with the payment' });
+  }
 };
 
-export default ctrl;
+customerCtrl.pay = async (req, res) => {
+  const { stripe, order } = req.body;
+  try {
+    await stripeAccount.charges.create({ stripe });
+    const orderConfirmation = {
+      ...mockPaymentConfirmation,
+      orderId: Math.floor(Math.random() * 1000),
+    };
+    res.status(200);
+    res.send(orderConfirmation);
+  } catch (e) {
+    console.log(`Something went wrong with the payment ${order}: `, e);
+    res.status(500);
+    res.send({ Error: 'Something went wrong with the payment' });
+  }
+};
+
+export default customerCtrl;
--- models/customer/index.js
+++ models/customer/index.js
@@ -2,8 +2,45 @@
 
 const customerModel = {};
 
-customerModel.getMenu = (barId) => {
-  console.log('Success! Fetching the menu for: ', barId);
+const mockMenu = {
+  barId: 'a791xu',
+  name: 'Cheers',
+  currency: 'GBP',
+  vat: 0.13,
+  open: 'true',
+  menu: [
+    {
+      name: 'Beers',
+      items: [
+        {
+          name: 'Budweiser',
+          price: 2.99,
+        },
+        {
+          name: 'Coors',
+          price: 3.5,
+        },
+      ],
+    },
+    {
+      name: 'Cocktails',
+      items: [
+        {
+          name: 'Dark n Stormy',
+          price: 5.99,
+        },
+        {
+          name: 'Long Island Iced Tea',
+          price: 4.25,
+        },
+      ],
+    },
+  ],
 };
+
+customerModel.getMenu = barId => ({
+  ...mockMenu,
+  barId,
+});
 
 export default customerModel;
